7b356c3fb7301dd6f066109808a668a9c5fc5715
UmaisRehman <umaisrehman109@gmail.com>
2024-06-30 16:25:26 +0500

    marsheek done

---
+++ app.js
@@ -0,0 +1,40 @@
+// // console.log(5 = 5)
+
+// // var num1 = 10;
+// // num1 += 20;
+// // console.log("+",num1);
+
+// // num1 -= 20;
+// // console.log("-",num1);
+
+// // num1 *= 20;
+// // console.log("*",num1);
+
+// // num1 /= 20;
+// // console.log("/",num1);
+
+// console.log("Cmparision Operators")
+// var num1 = 20>=20;
+// console.log( num1);
+
+console.log("mark sheet");
+var totalmarks = 400;
+var mathsub = +prompt("Math Obtain Marks");
+var Physsub = +prompt("Math Obtain Marks");
+var chemsub = +prompt("Math Obtain Marks");
+var engsub = +prompt("Math Obtain Marks");
+
+var obtainmarks = mathsub + Physsub + chemsub + engsub;
+
+var percentage = obtainmarks /totalmarks * 100;
+
+console.log(mathsub)    
+console.log(Physsub)
+console.log(chemsub)
+console.log(engsub)
+console.log("You Got: "+ percentage +"%")
+
+
+
+
+
